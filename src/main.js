7d84566ab030642138c8f58a5264f0044fc2926b
liuh <liuh@g-bits.com>
2021-06-04 21:00:08 +0800

    调整代码

--- src/main.js
+++ src/main.js
@@ -1,36 +1,38 @@
 import * as THREE from 'three';
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+(async () => {
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
-camera.position.set(0, 0, 100);
-camera.lookAt(0, 0, 0);
+    const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+    camera.position.set(0, 0, 100);
+    camera.lookAt(0, 0, 0);
 
+    const scene = new THREE.Scene();
 
-const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+    // const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
 
-const points = [];
-points.push(new THREE.Vector3(- 10, 0, 0));
-points.push(new THREE.Vector3(0, 20, 0));
-points.push(new THREE.Vector3(10, 0, 0));
+    // const points = [];
+    // points.push(new THREE.Vector3(- 10, 0, 0));
+    // points.push(new THREE.Vector3(0, 20, 0));
+    // points.push(new THREE.Vector3(10, 0, 0));
 
-const geometry = new THREE.BufferGeometry().setFromPoints(points);
+    // const geometry = new THREE.BufferGeometry().setFromPoints(points);
 
-const line = new THREE.Line(geometry, material);
-const scene = new THREE.Scene();
-scene.add(line);
+    // const line = new THREE.Line(geometry, material);
+    // scene.add(line);
 
-function showSprite(spritePath, s) {
-    const map = new THREE.TextureLoader().load(spritePath);
-    const material = new THREE.SpriteMaterial({ map: map });
+    async function showSprite(spritePath, s) {
+        const map = await (new THREE.TextureLoader()).loadAsync(spritePath);
+        const material = new THREE.SpriteMaterial({ map: map });
 
-    const sprite = new THREE.Sprite(material);
-    s.add(sprite);
-    sprite.scale.set(10, 10, 10);
-}
+        const sprite = new THREE.Sprite(material);
+        sprite.scale.set(10, 10, 1);
+        s.add(sprite);
+    }
 
-showSprite("textures/star.png", scene);
+    await showSprite("textures/star.png", scene);
 
-renderer.render(scene, camera);
+    renderer.render(scene, camera);
+})();
